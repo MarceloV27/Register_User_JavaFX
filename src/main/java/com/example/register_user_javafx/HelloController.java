5692483e51fc54a71b4d5521dfa6677b7ad51f34
marce <marceloveintimilla3@gmail.com>
2024-06-24 14:49:36 -0400

    First commit Init

---
+++ src/main/java/com/example/register_user_javafx/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.register_user_javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelloController {
+
+    @FXML
+    private Button idRegister;
+
+    @FXML
+    private Button idReset;
+
+
+
+
+
+}
